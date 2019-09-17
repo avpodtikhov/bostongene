c3053b9669a9fe0c24da866277867499497f12cd
avpodtikhov <34601714+avpodtikhov@users.noreply.github.com>
2019-09-17 22:25:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Config(object):
     CELERY_BROKER_URL='redis://localhost:6379/0',
     CELERY_RESULT_BACKEND='redis://localhost:6379/0'
 
-    MAIL_USERNAME = 'apodtikhov@gmail.com'
-    MAIL_PASSWORD = 'vova315362'
+    MAIL_USERNAME = '<gmail>'
+    MAIL_PASSWORD = '<password>'
     MAIL_SERVER='smtp.gmail.com'
     MAIL_PORT=465
     MAIL_USE_SSL=True
